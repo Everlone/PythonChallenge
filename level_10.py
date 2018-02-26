c10fa255ae757a8f8d26df591029a07f53cf7f2a
Stephen Outten <stephen.outten@googlemail.com>
2018-02-26 23:36:08 +0100

    Updated level 10

--- level_10.py
+++ level_10.py
@@ -1,27 +1,36 @@
+'''
+level_10.py
+Python Challenge level 10
+Complete series that describes elements.
+Convert the string into an array of digits and identify where changes occur.
+Loop over changes and separate string into blocks.
+Build new string out of length and value from each block.
+Append to array of series and check length of 30th element.
 
-numstr = "11"
-series_length = 30
-series = ["1"]
+Better solution uses Re, but the patten is very complicated.
+  for m in re.finditer(r"(\d)\1*", a): print(m)
 
-for i in range(1,31):     # series is 30 long since 0 to 29
-    count = 0
-    chrstr = ""
-    nextstr = ""
-    series.insert(i,numstr)
-    for j in range(0,len(numstr)):
-        if chrstr == "":
-            chrstr = numstr[j]
-            count += 1
-        elif chrstr == numstr[j]:
-            count += 1
-        else:
-            nextstr = nextstr + str(count) + chrstr
-            chrstr = numstr[j]
-            count = 1
-    nextstr = nextstr + str(count) + chrstr
-    numstr = nextstr
+For example:
+  "".join([str(len(m[0]))+m[0][0] for m in re.finditer(r"(\d)\1*", a)])
 
-print(series)
-print("length of series is ",len(series))
-print("\r")
-print("Answer is len(a[30]) = ",len(series[30]))
+'''
+
+import numpy as np
+
+series = ['1']
+
+for element in range(31):
+	oldstr = series[-1]
+	numbers = np.array(list(map(int,oldstr)))
+	change_locs = np.where(numbers[1:]-numbers[0:-1]!=0)[0] + 1
+	change_locs = np.concatenate(([0],change_locs,[len(oldstr)]))
+
+	newstr = ''
+	for i in range(len(change_locs)-1):
+		block = oldstr[change_locs[i]:change_locs[i+1]]
+		newstr = newstr + str(len(block)) + block[0]
+	print(newstr)
+	series.append(newstr)
+
+print('Answer is', len(series[30]))
+
